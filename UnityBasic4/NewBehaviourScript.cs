24cc095a7fbf803fc52211f399eaacae92b07e8e
star99090 <star99090@naver.com>
2020-04-07 22:16:37 +0900

    UnityBasic5

---
+++ UnityBasic4/NewBehaviourScript.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int level = 5;
+    int health = 30;
+    int mana = 15;
+    float strength = 15.5f;
+    int exp = 1500;
+    string title = "전설의";
+    string playerName = "나검사";
+    bool isFullLevel = false;
+
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+
+        //1.변수
+
+        /*
+         * Debug.Log("용사의 이름은?");
+        Debug.Log(playerName);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strength);
+        Debug.Log("용사는 만렙인가?");
+        Debug.Log(isFullLevel);
+        */
+
+        //2.그룹형 변수
+        string[] monsters = { "슬라임", "사막뱀", "악마" };
+        int[] monsterLevel = new int[3];
+        monsterLevel[0] = 1;
+        monsterLevel[1] = 6;
+        monsterLevel[2] = 20;
+
+        List<string> items = new List<string>();
+        items.Add("생명물약30");
+        items.Add("마나물약30");
+
+        //3.연산자
+
+        exp = 1500 + 320;
+        exp = exp - 10;
+        level = exp / 300;
+        strength = level * 3.1f;
+
+        int nextExp = 300 - (exp % 300);
+
+        /*
+        Debug.Log("용사의 이름은?");
+        Debug.Log(title+" "+playerName);
+        */
+        int fullLevel = 99;
+        isFullLevel = level == fullLevel;
+
+        bool isEndTutorial = level > 10;
+
+        bool isBadCondition = health <= 50 || mana <= 20;
+
+        string condition = isBadCondition ? "나쁨" : "좋음";
+
+        //4.키워드
+        //int float string bool new List
+
+        //5.조건문
+        if(condition == "나쁨")
+        {
+            //Debug.Log("플레이어의 상태가 나쁘니 아이템을 사용하세요.");
+        }
+        else
+        {
+            //Debug.Log("플레이어의 상태가 좋습니다.");
+        }
+
+        if(isBadCondition&&items[0] =="생명물약30")
+        {
+            items.RemoveAt(0);
+            health += 30;
+        }
+        else if(isBadCondition && items[0]=="마나물약30")
+        {
+            items.RemoveAt(0);
+            mana += 30;
+        }
+
+        string monsterAlarm;
+        switch(monsters[1])
+        {
+            case "슬라임":
+            case "사막뱀":
+                //Debug.Log("소형 몬스터가 출현!");
+                break;
+            case "악마":
+                //Debug.Log("중형 몬스터가 출현!");
+                break;
+            case "골렘":
+                //Debug.Log("대형 몬스터가 출현!");
+                break;
+            default:
+                //Debug.Log("??? 몬스터가 출현!");
+                break;
+        }
+
+        //6. 반복문
+        while (health > 0)
+        {
+            health--;
+
+            if (health == 10)
+                break;
+        }
+
+        for (int count = 0; count < 10; count++)
+                health++;
+        
+        foreach (string monster in monsters)
+        {
+                //Debug.Log("이 지역에 있는 몬스터 : " + monster);
+            }
+
+        //7.함수(메소드)
+        Heal();
+
+        for(int i=0; i<monsters.Length; i++)
+        {
+           // Debug.Log("용사는" + monsters[i] + "에게 " +
+           //     Battle(monsterLevel[i]));
+        }
+
+        //8.클래스
+        Player player = new Player();
+        player.id = 0;
+        player.name = "나법사";
+        player.title = "현명한";
+        player.strength = 2.4f;
+        player.weapon = "나무 지팡이";
+        Debug.Log(player.Talk());
+        Debug.Log(player.HasWeapon());
+
+        player.LevelUp();
+        Debug.Log(player.name + "의 레벨은 " + player.level + "입니다.");
+        Debug.Log(player.move());
+    }
+
+    void Heal()
+    {
+        health += 10;
+    }
+
+    string Battle(int monsterLevel)
+    {
+        string result;
+        if (level >= monsterLevel)
+            result = "이겼습니다.";
+        else
+            result = "졌습니다.";
+
+        return result;
+    }
+}
---
+++ UnityBasic5/LifeCycle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//초기화-(활성화)-물리-게임로직-(비활성화)-해체
+//          └ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ┘
+
+public class LifeCycle : MonoBehaviour
+{
+    void Awake() // 게임 오브젝트 생성할 때, 최초 실행 1번
+    {
+        Debug.Log("플레이의 데이터가 준비되었습니다.");
+    }
+
+    void OnEnable() // 게임 오브젝트가 활성화 되었을 때
+    {
+        Debug.Log("플레이어가 로그인했습니다.");
+    }
+
+    void Start() // 업데이트 시작 직전, 최초 실행 1번
+    {
+        Debug.Log("사냥 장비를 챙겼습니다.");
+    }
+
+    void FixedUpdate() // 물리 연산 업데이트, 고정된 실행 주기(50/s)로 CPU 많이 사용
+                               // = CPU 부하가 많이 생길 수 있다
+    {
+        Debug.Log("이동~");
+    }
+
+    void Update() // 게임 로직 업데이트, 환경에 따라 실행주기가 떨어질 수 있음
+                  // 내 노트북에선 이게 훨씬 빠름
+    {
+        Debug.Log("몬스터 사냥!");
+    }
+
+    void LateUpdate() // 모든 업데이트 끝난 후, 로직의 후처리
+                      // 카메라 이동이나 경험치 획득 등을 많이 넣는 편
+    {
+        Debug.Log("경험치 획득.");
+    }
+
+    void OnDisable() // 게임 오브젝트가 비활성화 되었을 때
+    {
+        Debug.Log("플레이어가 로그아웃했습니다.");
+    }
+
+    void OnDestroy() // 게임 오브젝트가 삭제되기 직전
+    {
+        Debug.Log("플레이어 데이터를 해제하였습니다.");
+    }
+}
