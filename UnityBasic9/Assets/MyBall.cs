bd78d07ddeadb67d9790e5add3950c67c81ee620
star99090 <star99090@naver.com>
2020-04-19 21:14:52 +0900

    UnityBasic11

--- UnityBasic9/Assets/MyBall.cs
+++ UnityBasic9/Assets/MyBall.cs
@@ -15,9 +15,7 @@ public class MyBall : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate()
-    {
-        
-        
+    {   /*
         if(Input.GetButtonDown("Jump"))
         {
             rigid.AddForce(Vector3.up * 5, ForceMode.Impulse); //2. 힘을 줘서 움직이기
@@ -25,10 +23,11 @@ public class MyBall : MonoBehaviour
             //캐릭터 점프는 보통 rigid.AddForce(Vector3.Up * 5, ForceMode.Impulse) 이거로 씀
             //Mass(무게)의 영향을 받는다.
         }
-        
-        Vector3 vec = new Vector3(Input.GetAxisRaw("Horizontal"),
-                                  0,
-                                  Input.GetAxisRaw("Vertical"));
+        */
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector3 vec = new Vector3(h, 0, v);
+
         rigid.AddForce(vec, ForceMode.Impulse); //이게 있어야 rigid에 힘이 추가되는 듯
         //rigid.velocity = new Vector3(2,4,3); //1. 속력 바꾸기
         //Update()에서 호출하면 계속 이동함(로켓 마냥) velocity : 현재 이동속도
@@ -36,6 +35,25 @@ public class MyBall : MonoBehaviour
         
 
         //3. 회전력 : Vec 방향을 '축'으로 회전력이 생김
-        rigid.AddTorque(Vector3.down);
+        //rigid.AddTorque(Vector3.down);
+    }
+
+    private void OnTriggerStay(Collider other) // 콜라이더가 계속 충돌하고 있을 때 호출
+    {
+        if(other.name == "Cube")
+        {
+            rigid.AddForce(Vector3.up * 2, ForceMode.Impulse);
+        }
     }
 }
+/*
+//실제 물리적인 충돌로 발생하는 이벤트
+void OnCollisionEnter(Collision collision) { }
+void OnCollisionStay(Collision collision) { }
+void OnCollisionExit(Collision collision) { }
+
+//콜라이더 충돌로 발생하는 이벤트
+void OnTriggerEnter(Collider other) { }
+void OnTriggerStya(Collider other) { }
+void OnTriggerExit(Collider other) { }
+*/
---
+++ UnityBasic9/Assets/OtherBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherBall : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+        
+    }
+    //이벤트 함수 만들때는 보통 앞에 On을 붙인다, Collision : 충돌 정보 클래스
+    private void OnCollisionEnter(Collision collision) // 물리적 충돌이 시작할 때 호출되는 함수
+    {
+        if(collision.gameObject.name == "MyBall") 
+            mat.color = new Color(0, 0, 0); // Color32로 하면 255가지 그림판 그 색상 나옴 ㅎ
+    }
+
+    /*
+    private void OnCollisionStay(Collision collision) // 물리적 충돌이 진행되는 중
+    {
+
+    }
+    */
+
+    private void OnCollisionExit(Collision collision) // 물리적 충돌이 끝났을 때 호출되는 함수
+    {
+        if (collision.gameObject.name == "MyBall")
+            mat.color = new Color(1, 1, 1);
+    }
+    
+}
